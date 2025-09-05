1567dae4ed8d645a1bf450d64658e3cad6fe4bb9
Levent YADIRGA <lyadirga@gmail.com>
2025-09-05 22:37:50 +0300

    guide güncellendi, bilgi ekranı dolduruldu

--- app/src/main/java/com/lyadirga/bildirimleogren/util/Constants.kt
+++ app/src/main/java/com/lyadirga/bildirimleogren/util/Constants.kt
@@ -2,5 +2,4 @@ package com.lyadirga.bildirimleogren.util
 
 
 
-//TODO: değiştir
-const val GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdJU8qyKU3szHgVxxlZnaTqBdggME3YaBfFq0lbOXRlhDkKpQ/viewform?usp=pp_url"
+const val GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeBcfoT_eMMj6z1sG7P4t2_AAzd49bUiII2nQ8GzdZ2BQTr6A/viewform?usp=header"
